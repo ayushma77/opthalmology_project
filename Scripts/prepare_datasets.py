cf0e7088a54a67a2cd31d112c68ace4b8d910a73
ayushma77 <pandeyayushma77@gmail.com>
2024-01-03 16:05:16 +0545

    label

--- Scripts/prepare_datasets.py
+++ Scripts/prepare_datasets.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -8,40 +9,35 @@ excel_file_path=r"data\ODIR-5K_Training_Annotations(Updated)_V2.xlsx"
 annoted_data=pd.read_excel(excel_file_path)
 
 csv_data=annoted_data.to_csv(os.path.join(BASE_DIR, "data.csv"), index=False)
-
-# left_data = annoted_data[['Left-Fundus', 'Left-Diagnostic Keywords']].copy()
-# left_data.columns = ['Image', 'Label']
-# right_data = annoted_data[['Right-Fundus', 'Right-Diagnostic Keywords']].copy()
-# right_data.columns = ['Image', 'Label']
-
-# # Concatenate left and right data
-# merged_data = pd.concat([left_data, right_data], ignore_index=True)
-# train_csv=merged_data.to_csv(os.path.join(BASE_DIR, "train.csv"), index=False)
-
 data = annoted_data[['Left-Fundus','Right-Fundus', 'Left-Diagnostic Keywords', 'Right-Diagnostic Keywords','N','D','G','C','A','H','M','O']].copy()
-csv=data.to_csv(os.path.join(BASE_DIR, "data1.csv"), index=False)
+csv_data1=data.to_csv(os.path.join(BASE_DIR, "data1.csv"), index=False)
+data1 = pd.read_csv(os.path.join(BASE_DIR, "data1.csv"))
 
 
-import csv
+
+unique_left_labels = pd.unique(data1['Left-Diagnostic Keywords'])
+unique_right_labels = pd.unique(data1['Right-Diagnostic Keywords'])
+
+
+print("Unique Left Labels:", unique_left_labels)
+print("Unique Right Labels:", unique_right_labels)
 
 # Initialize lists to store left image paths and right image paths
 left_image_paths = []
 right_image_paths = []
 labels = []
 
-# Assuming 'data.csv' is the file containing your data
-with open('data/data1.csv', newline='') as csvfile:
+with open(os.path.join(BASE_DIR, "data1.csv"), newline='') as csvfile:
     reader = csv.reader(csvfile)
-    next(reader)  # Skip header row if it exists
+    next(reader) 
     for row in reader:
         left_image_path, right_image_path, left_diagnostic, right_diagnostic, *_ = row
         
-        # Check if the condition is met for 'cataract' and 'normal fundus'
         if left_diagnostic.lower() == 'cataract' and right_diagnostic.lower() == 'normal fundus':
             left_image_paths.append(left_image_path)
             right_image_paths.append(right_image_path)
             labels.append('cataract')
-        elif left_diagnostic.lower() == 'normal' and right_diagnostic.lower() == 'contaract':
+        elif left_diagnostic.lower() == 'normal' and right_diagnostic.lower() == 'cataract':
             left_image_paths.append(left_image_path)
             right_image_paths.append(right_image_path)
             labels.append('cataract')
@@ -50,7 +46,6 @@ with open('data/data1.csv', newline='') as csvfile:
             left_image_paths.append(left_image_path)
             right_image_paths.append(right_image_path)
             labels.append('other disease')
-        # Check if the condition is met for 'normal fundus' and 'normal fundus'
         elif left_diagnostic.lower() == 'normal fundus' and right_diagnostic.lower() == 'normal fundus':
             left_image_paths.append(left_image_path)
             right_image_paths.append(right_image_path)
@@ -125,14 +120,15 @@ with open('data/data1.csv', newline='') as csvfile:
             left_image_paths.append(left_image_path)
             right_image_paths.append(right_image_path)
             labels.append('hypertension')
-            
-            
-# Write the image paths and labels to a new CSV file
+        # elif left_diagnostic.lower() == 'laser spot' and left_diagnostic.lower() == 'moderate non proliferative retinopathy'  and   right_diagnostic.lower()=='moderate non proliferative retinopathy':
+        #     left_image_paths.append(left_image_path)
+        #     right_image_paths.append(right_image_path)
+        #     labels.append('diabetes')
+
+
 with open('output.csv', mode='w', newline='') as output_file:
     writer = csv.writer(output_file)
-    writer.writerow(['Image Path', 'Label'])  # Write header
+    writer.writerow(['Image Path', 'Label'])  
     for left_path, right_path, label in zip(left_image_paths, right_image_paths, labels):
         writer.writerow([left_path, label])
         writer.writerow([right_path, label])
-
-print("CSV file 'output.csv' created with image paths and labels.")
