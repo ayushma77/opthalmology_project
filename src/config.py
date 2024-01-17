883bf91006a2be9013a47504aac1637434e0d2e6
ayushma77 <pandeyayushma77@gmail.com>
2024-01-17 15:30:23 +0545

    updated traincsv path

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-TRAIN_CSV=r"data\processed_train_ODIR-5K.csv"
-TEST_CSV=r"data\processed_test_ODIR-5k.csv"
-VAL_CSV=r"data\processed_val_ODIR-5K.csv"
+TRAIN_CSV=r"/content/opthalmology_project/data/train_ODIR-5K.csv"
+TEST_CSV=r"/content/opthalmology_project/data/test_ODIR-5k.csv"
+VAL_CSV=r"/content/opthalmology_project/data/val_ODIR-5K.csv"
 BATCH_SIZE=32
 LR=0.001
 EPOCHS=50
