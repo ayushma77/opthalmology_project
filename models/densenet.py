a6f84ebf6bce764293e9cd86392589d3aa83845f
ayushma77 <pandeyayushma77@gmail.com>
2024-01-17 15:48:00 +0545

    dense net

--- models/densenet.py
+++ models/densenet.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 from torchsummary import summary
 from torchvision import models
 import torch
+
+
 class DenseNetModel(nn.Module):
     def __init__(self, num_labels):
         super(DenseNetModel, self).__init__()
@@ -15,7 +17,7 @@ class DenseNetModel(nn.Module):
 
         # Replace the final layer of the classifier
         in_features = self.densenet.classifier.in_features
-        
+
         self.densenet.classifier = nn.Sequential(
             nn.Linear(in_features, 512),
             nn.ReLU(inplace=True),
@@ -23,23 +25,20 @@ class DenseNetModel(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(256, 128),
             nn.ReLU(inplace=True),
-            nn.Linear(128, num_labels)
+            nn.Linear(128, num_labels),
         )
 
     def forward(self, x):
         return self.densenet(x)
 
-if __name__ == "__main__":
-    # Create an instance of the model
-    model = DenseNetModel(num_labels=8)
-    # Move the model to GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
-    # Create a dummy input tensor and move it to GPU if available
-    dummy_input = torch.randn(1, 3, 256, 256).to(device)
+# if __name__ == "__main__":
+#     model = DenseNetModel(num_labels=8)
+#     # Move the model to GPU
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     model.to(device)
 
-    # Use torchsummary to print the model summary
-    summary(model, input_size=(3, 256, 256))
-
-   
+#     x = torch.rand(1,3,224,224).to(device)
+#     o = model(x)
+#     print(o)
+#     summary(model, input_size=(3, 224, 224))
