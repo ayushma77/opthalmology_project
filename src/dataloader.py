b1ddd9f5dea0231cddef8b4e09930642a71881bb
ayushma77 <pandeyayushma77@gmail.com>
2024-01-18 18:02:17 +0545

    updating

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 from torch.utils.data import DataLoader
 
-from config import BATCH_SIZE, EPOCHS, LR, SEED, TEST_CSV, TRAIN_CSV, VAL_CSV
-from datasets import DRDataset
-from transforms import transforms
+from src.config import (BATCH_SIZE, EPOCHS, LR, SEED, TEST_CSV, TRAIN_CSV,
+                        VAL_CSV)
+from src.datasets import DRDataset
+from src.transforms import transforms
 
 train_dataset = DRDataset(csv_path=TRAIN_CSV, transforms=transforms,has_labels=True)
 val_dataset = DRDataset(csv_path=VAL_CSV, transforms=transforms,has_labels=True)
