7a6a8839b75f894a589b0d4c9e49249d96651088
ayushma77 <pandeyayushma77@gmail.com>
2024-01-18 14:37:41 +0545

    updated

--- src/dataloader.py
+++ src/dataloader.py
@@ -15,8 +15,8 @@ def check_file_existence(file_path):
         print(f"File not found: {file_path}")
 
 # Check file existence for each CSV file
-check_file_existence("/content/opthalmology_project/data/processed_train_ODIR-5k.csv")
-check_file_existence("/content/opthalmology_project/data/processed_val_ODIR-5k.csv")
+check_file_existence("/content/opthalmology_project/data/processed_train_ODIR-5K.csv")
+check_file_existence("/content/opthalmology_project/data/processed_val_ODIR-5K.csv")
 check_file_existence("/content/opthalmology_project/data/processed_test_ODIR-5k.csv")
 
 train_dataset = DRDataset(csv_path=TRAIN_CSV, transforms=transforms,has_labels=True)
