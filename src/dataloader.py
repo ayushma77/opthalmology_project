283f35738b94e96d312c69264bfc03bb892ee823
ayushma77 <pandeyayushma77@gmail.com>
2024-01-18 09:34:08 +0545

    path verification  updated

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,6 +4,11 @@ from config import BATCH_SIZE, EPOCHS, LR, SEED, TEST_CSV, TRAIN_CSV, VAL_CSV
 from datasets import DRDataset
 from transforms import transforms
 
+# Print paths for verification
+print("TRAIN_CSV:", TRAIN_CSV)
+print("VAL_CSV:", VAL_CSV)
+print("TEST_CSV:", TEST_CSV)
+
 train_dataset = DRDataset(csv_path=TRAIN_CSV, transforms=transforms,has_labels=True)
 val_dataset = DRDataset(csv_path=VAL_CSV, transforms=transforms,has_labels=True)
 test_dataset = DRDataset(csv_path=TEST_CSV, transforms=transforms, has_labels=False)
