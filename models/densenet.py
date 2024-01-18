8faf2ed20f7e57193e25a58e111f38831a2efa28
ayushma77 <pandeyayushma77@gmail.com>
2024-01-18 06:07:17 +0545

    edited in src.datasets

--- models/densenet.py
+++ models/densenet.py
@@ -1,7 +1,7 @@
+import torch
 import torch.nn as nn
 from torchsummary import summary
 from torchvision import models
-import torch
 
 
 class DenseNetModel(nn.Module):
--- src/dataloader.py
+++ src/dataloader.py
@@ -1,8 +1,9 @@
 from torch.utils.data import DataLoader
 
-from src.datasets import DRDataset
-from src.transforms import transforms
-from src.config import  EPOCHS,BATCH_SIZE,TEST_CSV,TRAIN_CSV,VAL_CSV,LR,SEED
+from datasets import DRDataset
+from src.config import (BATCH_SIZE, EPOCHS, LR, SEED, TEST_CSV, TRAIN_CSV,
+                        VAL_CSV)
+from transforms import transforms
 
 train_dataset = DRDataset(csv_path=TRAIN_CSV, transforms=transforms,has_labels=True)
 val_dataset = DRDataset(csv_path=VAL_CSV, transforms=transforms,has_labels=True)
