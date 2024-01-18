74fdcd9ee91b0b822daed8d0aa495a81d1b170f0
ayushma77 <pandeyayushma77@gmail.com>
2024-01-18 09:31:58 +0545

    path updated

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-TRAIN_CSV=r"/content/opthalmology_project/data/processed_train_ODIR-5k.csv"
-TEST_CSV=r"/content/opthalmology_project/data/processed_test_ODIR-5k.csv"
-VAL_CSV=r"/content/opthalmology_project/data/processed_val_ODIR-5k.csv"
+TRAIN_CSV="/content/opthalmology_project/data/processed_train_ODIR-5k.csv"
+TEST_CSV="/content/opthalmology_project/data/processed_test_ODIR-5k.csv"
+VAL_CSV="/content/opthalmology_project/data/processed_val_ODIR-5k.csv"
 BATCH_SIZE=32
 LR=0.001
 EPOCHS=50
